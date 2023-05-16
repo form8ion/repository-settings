4ecd76926895efdd92704436ebbab8dc4cf63837
Matt Travi <programmer@travi.org>
2023-05-15 21:41:00 -0500

    feat: extracted implementation from `@travi/github-scaffolder`
    extracted as of
    https://github.com/travi/github-scaffolder/commit/5c4cfb51e0477aa998fb0f4255dd33470645a8fc

--- test/integration/features/step_definitions/common-steps.js
+++ test/integration/features/step_definitions/common-steps.js
@@ -1,11 +1,21 @@
 import {dirname, resolve} from 'node:path';
 import {fileURLToPath} from 'node:url';
 
-import {After, When} from '@cucumber/cucumber';
+import {After, Before, When} from '@cucumber/cucumber';
 import stubbedFs from 'mock-fs';
+import any from '@travi/any';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));          // eslint-disable-line no-underscore-dangle
 const stubbedNodeModules = stubbedFs.load(resolve(__dirname, '..', '..', '..', '..', 'node_modules'));
+
+Before(function () {
+  this.projectRoot = process.cwd();
+  this.projectName = any.word();
+  this.projectDescription = any.sentence();
+  this.projectHomepage = any.url();
+  this.projectVisibility = any.fromList(['Public', 'Private']);
+  this.topics = any.listOf(any.word);
+});
 
 After(function () {
   stubbedFs.restore();
@@ -19,5 +29,12 @@ When('the project is scaffolded', async function () {
     node_modules: stubbedNodeModules
   });
 
-  await scaffold({projectRoot: process.cwd()});
+  await scaffold({
+    projectRoot: this.projectRoot,
+    projectName: this.projectName,
+    description: this.projectDescription,
+    homepage: this.projectHomepage,
+    visibility: this.projectVisibility,
+    topics: this.topics
+  });
 });
